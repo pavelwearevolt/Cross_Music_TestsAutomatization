3951f545e759b19ca46b295a0002e0e6da8522be
Pavel Kosicin <pavel.kosicin@wearevolt.com>
2018-01-16 19:26:32 +0300

    создание общей фикстуры (одна фикстура для всех тестов), переименование теста

--- conftest.py
+++ conftest.py
@@ -9,9 +9,3 @@ def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
-
-
-def test_first_test(app):
-    app.session.login(username="pavel.kosicin@wearevolt.com", password="abcd1234")
-    app.search.enter_query(query="perfect")
-    app.session.logout()
